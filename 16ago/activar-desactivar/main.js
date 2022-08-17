f850e1e1cf6d1b77a4349915c872ebdb584d6f94
booleanchile <46203382+booleanchile@users.noreply.github.com>
2022-08-17 15:20:13 -0400

    Actualizacion clase 17 ago

--- 16ago/activar-desactivar/main.js
+++ 16ago/activar-desactivar/main.js
@@ -4,3 +4,13 @@
 2. Si hago click en el boton desactivar
   1.1 Se cambia la clase del div resultado por inactivo (seleccionamos el elemento, cambiamos la clase)
 */
+
+const activarDiv = () => {
+  const div = document.getElementById('resultado')
+  div.className = 'activo'
+}
+
+const desactivarDiv = () => {
+  const div = document.getElementById('resultado')
+  div.className = 'inactivo'
+}
