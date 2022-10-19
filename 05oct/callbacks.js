26bd42f2ab2dd97b21782775dde7c76c3fdd1bfc
booleanchile <46203382+booleanchile@users.noreply.github.com>
2022-10-19 12:18:27 -0300

    Renombrando carpeta

---
+++ 05oct/callbacks.js
@@ -0,0 +1,13 @@
+//   <img src="" alt="Pokemon" id="pokemon-image">
+
+const cuandoLlegueElPokemon = (pokemon) => {
+  console.log(pokemon.sprites.front_default)
+  const image = document.getElementById("pokemon-image")
+  image.src = pokemon.sprites.front_default
+}
+
+const pokemonName = prompt('Elige tu pokemon:')
+
+fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
+  .then(response => response.json())
+  .then(cuandoLlegueElPokemon);
